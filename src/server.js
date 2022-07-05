a864b0a2f1481b88e445fbf8dde3cbf32ecaa011
jang <dkanvk1@gmail.com>
2022-07-05 23:36:14 +0900

    22.7.5

---
+++ src/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import morgan from "morgan";
+
+const app = express();
+const PORT = 4000;
+const logger = morgan("dev");
+
+const home = (req, res) => {
+  return res.send("hello");
+};
+
+const login = (req, res) => {
+  return res.send("login");
+};
+
+app.use(logger);
+app.get("/", home);
+app.get("/login", login);
+
+const handleListening = () => {
+  console.log(`✅Server listening on port http://localhost:${PORT}`);
+};
+
+app.listen(PORT, handleListening);
