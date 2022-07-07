0027a4d5df6969ee4faab48c2853d2e657cd03c5
jang <dkanvk1@gmail.com>
2022-07-08 02:18:04 +0900

    22.7.8

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,10 +1,36 @@
 const fakeUser = {
-  username: "Nicolas",
-  loggedIn: false,
+  username: "jk",
+  loggedIn: true,
 };
-
-
-export const trending = (req, res) => res.render("home", {pageTitle: "Home", fakeUser});
+const videos = [
+  {
+    title: "First Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 1,
+  },
+  {
+    title: "Second Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 1,
+  },
+  {
+    title: "Third Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 1,
+  },
+];
+export const trending = (req, res) => {
+  return res.render("home", { pageTitle: "Home", videos });
+};
 export const watch = (req, res) => res.render("watch");
 export const see = (req, res) => {
   return res.send(`Watch Video #${req.params.id}`);
@@ -16,4 +42,17 @@ export const search = (req, res) => res.send("Video search");
 export const upload = (req, res) => res.send("uploading");
 export const remove = (req, res) => {
   return res.send("Delete Video");
+};
+
+export const getUpload = (req, res) =>
+  res.render("upload", { pageTitle: "Upload" });
+export const postUpload = (req, res) => {
+  const a = req.body;
+  const newVideo = {
+    halo: a,
+    rating: 0,
+    comments: 0,
+  };
+  videos.push(newVideo);
+  return res.redirect("/");
 };
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -4,16 +4,18 @@ import {
   edit,
   remove,
   upload,
-  trending
+  trending,
+  getUpload,
+  postUpload
 } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
 videoRouter.get("/trending", trending);
 videoRouter.get("/upload", upload);
-videoRouter.get("/:id", see); // id란? 동영상의 id를 말한다. // database와 같이 정리해야할 코드
-videoRouter.get("/:id/edit", edit);
-videoRouter.get("/:id/remove", remove);
-
+videoRouter.get("/:id(\\d+)", see); // id란? 동영상의 id를 말한다. // database와 같이 정리해야할 코드
+videoRouter.get("/:id(\\d+)/edit", edit);
+videoRouter.get("/:id(\\d+)/remove", remove);
+videoRouter.route("/Sexupload").get(getUpload).post(postUpload);
 
 export default videoRouter;
