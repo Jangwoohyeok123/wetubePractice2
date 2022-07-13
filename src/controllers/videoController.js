b6f44dca49ead9af2e7b228a00cd83162a311753
jang <dkanvk1@gmail.com>
2022-07-13 23:51:56 +0900

    22.7.13

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,29 +1,40 @@
 import Video from "../models/Video";
 
 export const home = async (req, res) => {
-  console.log("Start");
-  const videos = await Video.find({});
+  const videos = await Video.find({}).sort({ createdAt: "desc" });
   return res.render("home", { pageTitle: "Home", videos });
 };
 
 export const watch = async (req, res) => {
   const { id } = req.params;
-  try {
-    const video = await Video.findById(id);
-  } catch (error) {
-    console.log(error._message);
+  const video = await Video.findById(id);
+  if (!video) {
+    return res.render("404", { pageTitle: "Video not found.", video });
   }
-  return res.render("watch", { pageTitle: `Watching` });
+  return res.render("watch", { pageTitle: `Watching`, video });
 };
 
-export const getEdit = (req, res) => {
+export const getEdit = async (req, res) => {
   const { id } = req.params;
-  return res.render("edit", { pageTitle: `Editing` });
+  const video = await Video.findById(id);
+  if (!video) {
+    return res.render("404", { pageTitle: "Video not found.", video });
+  }
+  return res.render("edit", { pageTitle: `Edit: ${video.title}`, video });
 };
 
-export const postEdit = (req, res) => {
+export const postEdit = async (req, res) => {
   const { id } = req.params;
-  const { title } = req.body;
+  const { title, description, hashtags } = req.body;
+  const video = await Video.exists({ _id: id });
+  if (!video) {
+    return res.render("404", { pageTitle: "Video not found." });
+  }
+  await Video.findByIdAndUpdate(id, {
+    title,
+    description,
+    hashtags: Video.formatHashtags(hashtags),
+  });
   return res.redirect(`/videos/${id}`);
 };
 
@@ -37,8 +48,7 @@ export const postUpload = async (req, res) => {
     await Video.create({
       title,
       description,
-      createdAt: "lsdasdsad",
-      hashtags: hashtags.split(",").map(word => `#${word}`),
+      hashtags: Video.formatHashtags(hashtags),
       meta: {
         views: 0,
         rating: 0,
@@ -51,4 +61,17 @@ export const postUpload = async (req, res) => {
       errorMessage: error._message,
     });
   }
+};
+
+export const deleteVideo = async (req, res) => {
+  const { id } = req.params;
+  await Video.findByIdAndDelete(id);
+  return res.redirect("/");
+};
+
+export const search = (req, res) => {
+  const { keyword } = req.query;
+  if (keyword) {
+  }
+  return res.render("search", { pageTitle: "Search" });
 };
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -6,11 +6,13 @@ import {
   postEdit,
   getUpload,
   postUpload,
+  deleteVideo
 } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 videoRouter.get("/:id([0-9a-f]{24})", watch); // id란? 동영상의 id를 말한다. // database와 같이 정리해야할 코드
 videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
+videoRouter.route("/:id([0-9a-f]{24})/delete").get(deleteVideo);
 videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 export default videoRouter;
